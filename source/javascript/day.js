7e1f662018ba3a08cae15492e367d9fc1e9df402
Leigh McCulloch <leigh@mcchouse.com>
2016-02-03 06:59:17 -0800

    Moved inline JavaScript on day.html into day.js.
    What
    ===
    Moved inline JavaScript on day.html into day.js.
    
    Why
    ===
    To get rid of inline JavaScript.

--- source/javascript/day.js
+++ source/javascript/day.js
@@ -1,12 +1,17 @@
 (function() {
   var $textControlVerses;
+  var $metaDay;
 
   function init() {
     $textControlVerses = document.querySelectorAll('.text-controls-verses');
+    $metaDay = document.querySelector('meta[name="readprayrepeat:day"]');
 
     for (var i = 0; i < $textControlVerses.length; ++i) {
       $textControlVerses[i].addEventListener("click", onTextControlVersesClick);
     }
+
+    renderCurrentDate();
+  }
 
   function onTextControlVersesClick(e) {
     e.preventDefault();
@@ -23,6 +28,27 @@
         $text[i].className = $text[i].className.replace('show-verses', '');
       }
     }
+  }
+
+  function renderCurrentDate() {
+    var day = $metaDay.getAttribute("content");
+    var date = getDateForDay(day);
+    var $navCurrentDate = document.querySelectorAll('.nav-current-date');
+    for (var i = 0; i < $navCurrentDate.length; ++i) {
+      $navCurrentDate[i].innerText = date.toDateString();
+    }
+  }
+
+  function getDateForDay(day) {
+    var start = getStartDate();
+    var oneDay = 1000 * 60 * 60 * 24;
+    var daysInTime = oneDay * day;
+    return new Date(start.getTime() + daysInTime);
+  }
+
+  function getStartDate() {
+    var now = new Date();
+    return new Date(now.getFullYear(), 0, 0);
   }
 
   window.addEventListener('load', init);
