a5aefa136f856fd67d323842852f47156d4d08f8
Leigh McCulloch <leigh@mcchouse.com>
2016-04-02 23:55:29 -0700

    Added a reading time estimate.
    What
    ===
    Added a reading time estimate.
    
    Why
    ===
    To help inspire and give people an idea of how much time it will take
    them to read the passages ahead of diving in.

--- bible.go
+++ bible.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 type Bible interface {
 	Source() string
 	NameShort() string
@@ -12,4 +14,15 @@ type BiblePassage struct {
 	Html         string
 	TrackingCode string
 	Copyright    string
+}
+
+func (p *BiblePassage) TimeToReadInMinutes() int {
+	const READING_WORDS_PER_MINUTE = 220
+	text := p.Html
+	wordCount := strings.Count(text, " ")
+	wordsPerMinute := wordCount / READING_WORDS_PER_MINUTE
+	if wordsPerMinute == 0 {
+		wordsPerMinute = 1
+	}
+	return wordsPerMinute
 }
