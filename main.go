598810715c6bb48f60c5248fec3c1664f677461a
Leigh McCulloch <leigh@mcchouse.com>
2018-02-21 06:05:07 +0000

    Fix assets when run locally

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func main() {
 		if r.URL.Path == "/" {
 			templateIndex.ExecuteTemplate(w, "entry", pages)
 		} else {
-			fs := http.FileServer(http.Dir("build"))
+			fs := http.FileServer(http.Dir("build-assets"))
 			fs.ServeHTTP(w, r)
 		}
 	})
